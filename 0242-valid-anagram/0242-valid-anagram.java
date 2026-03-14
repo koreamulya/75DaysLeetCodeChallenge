b5eb3368d3567d9f073aec7ff2e053a4c7b565a8
Kore Amulya <koreamulya6@gmail.com>
2026-03-14 20:53:17 +0530

    Time: 6 ms (42.86%), Space: 44.2 MB (94.26%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        int[] count=new int[26];//optimal
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true;
+        }
+    }
+
+//BRUTE FORCE(SORTING)
+/*if(s.length()!=t.length()) return false;
+        char[] s1=s.toCharArray();
+        char[] t1=t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+
+        return Arrays.equals(s1,t1);*/
